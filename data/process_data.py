4d79e5fc56cfe4b9ed51a9af9356c77d84d09cc2
karantongay <karantongay@gmail.com>
2020-04-27 22:54:13 -0700

    Project Files

---
+++ data/process_data.py
@@ -0,0 +1,94 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """
+    This function reads the raw data from messages and categories csv 
+    files into dataframes and merges the two separate dataframes as one
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
+
+
+def clean_data(df):
+    """
+    This function performs data cleaning and transformation.
+    """
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+    categories.columns = categories.iloc[0].values
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x[:-2])
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str[-1]
+
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+    
+    # There are invalid values in the related colum
+    # Assuming that its a data labelling issue, an attempt is made to assume 2 as 1
+    categories.loc[categories['related'] == 2, 'related'] = 1
+    
+    # drop the original categories column from `df`
+    del df['categories']
+    
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+    
+    # drop duplicates
+    df.drop_duplicates(keep=False,inplace=True)
+    
+    return df
+
+
+def save_data(df, database_filename):
+    """
+    This function saves the cleaned and transformed data to
+    the indicated database.
+    """
+    engine = create_engine('sqlite:///' + database_filename)
+    df.to_sql('figure-eight', engine, index=False, if_exists = 'replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
---
+++ models/train_classifier.py
@@ -0,0 +1,149 @@
+import sys
+
+import pandas as pd
+import numpy as np
+import re
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import SGDClassifier
+from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score
+import pickle as pkl
+
+
+def load_data(database_filepath):
+    """
+    This function reads the data from database where the clean
+    data is stored.
+    Note: It is observed that the 'child_alone' category in the
+    data had only one class which was '0' and therefore, at the
+    time of loading the data, we are currently deleting that
+    category attribute. Although, to keep it scalable, the
+    attribute still persists in the database, just in case if new 
+    data comes in the future that is classified as child_alone.
+    """
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('figure-eight', engine)
+    # There is only one classification class for child_alone which is 0 which indicates that there is no message classified into this class.
+    del df['child_alone']
+    X = df.message.values
+    Y = df[np.delete(df.columns.values, [0,1,2,3])].values
+    category_names = np.delete(df.columns.values, [0,1,2,3])
+    return X,Y,category_names
+
+
+def tokenize(text):
+    """
+    This function tokenizes the input text and performs necessary cleaning.
+    """
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
+
+    tokens = word_tokenize(text)
+    lemmatizer = WordNetLemmatizer()
+
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+
+def build_model():
+    """
+    This function builds a pipeline model which makes use of:
+    a. CountVectorizer()
+    b. TfidfTransformer()
+    c. MultiOutputClassifier(SGDClassifier())
+    
+    Note: The parameters used to call the above steps in the
+    pipeline were chosen after performing validation tests
+    using multiple parameters with GridSearchCV.
+    
+    parameters_SGD = {
+     'vect__ngram_range': [(1, 2), (1, 3), (1, 4)],
+     'tfidf__use_idf': (True, False),
+     'clf__estimator__alpha': (1e-2, 1e-3, 1e-4, 1e-1),
+    }
+
+    cv = GridSearchCV(model, param_grid=parameters_SGD, n_jobs = -1)
+    cv.get_params().keys()
+    cv.fit(X_train, y_train)
+    y_pred = cv.predict(X_test)
+    
+    SGDClassifier is chosen after testing and tuning other
+    classfiers namely, RandomForest, NaiveBayes and k-NN.
+    
+    """
+    text_clf_SGD = Pipeline([
+        ('vect', CountVectorizer(ngram_range = (1,2))),
+         ('tfidf', TfidfTransformer(use_idf = True)),
+         ('clf', MultiOutputClassifier(SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None)))
+        ])
+    return text_clf_SGD
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    """
+    This function evaluates the new model and generates classification
+    report containing precision, recall, f-score and accuracy information
+    for individual classes.
+    """
+    y_pred = model.predict(X_test)
+    for x in range(0, len(category_names)):
+        print(category_names[x])
+        print(classification_report(Y_test[:,x], y_pred[:,x]))
+        print("Accuracy: " + str(accuracy_score(Y_test[:, x], y_pred[:, x])))
+
+
+def save_model(model, model_filepath):
+    """
+    This function packages the trained model into the pickle file.
+    """
+    # save the classifier
+    with open(model_filepath, 'wb') as fid:
+        pkl.dump(model, fid)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state = 42)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
