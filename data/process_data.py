246e5dcf9be5f3e2ad43a62e168ddb41e81a412d
karantongay <karantongay@gmail.com>
2020-04-29 12:13:30 -0700

    Update function/method docs

--- data/process_data.py
+++ data/process_data.py
@@ -6,7 +6,19 @@ from sqlalchemy import create_engine
 def load_data(messages_filepath, categories_filepath):
     """
     This function reads the raw data from messages and categories csv 
-    files into dataframes and merges the two separate dataframes as one
+    files into dataframes and merges the two separate dataframes as one.
+
+    Args:
+        messages_filepath: Path to the messages csv file
+        categories_filepath: Path to the categories csv file
+
+    Returns:
+        A pandas dataframe containing messages and categories dataframe
+        merged as a single dataframe joined together using 'id' column.
+
+    Raises:
+        The IOException: No such file or directory.
+        If any of the filepaths are invalid or non-existent.
     """
     messages = pd.read_csv(messages_filepath)
     categories = pd.read_csv(categories_filepath)
@@ -17,6 +29,13 @@ def load_data(messages_filepath, categories_filepath):
 def clean_data(df):
     """
     This function performs data cleaning and transformation.
+    It should be called after 'load_data' method is called.
+    Args:
+        df: A pandas dataframe with raw data.
+    
+    Returns:
+        df: A clean pandas dataframe after applying the specific
+        transformations on data.
     """
     # create a dataframe of the 36 individual category columns
     categories = df['categories'].str.split(';', expand=True)
@@ -59,6 +78,15 @@ def save_data(df, database_filename):
     """
     This function saves the cleaned and transformed data to
     the indicated database.
+
+    Args:
+        df: A pandas dataframe that contains clean data.
+        database_filename: Path of the database file to be created.
+    
+    Returns:
+        Nothing.
+        Creates a database containing a table named 'figure-eight'
+        with clean data.
     """
     engine = create_engine('sqlite:///' + database_filename)
     df.to_sql('figure-eight', engine, index=False, if_exists = 'replace')
